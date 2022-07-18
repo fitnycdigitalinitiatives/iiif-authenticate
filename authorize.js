bd49c460f11e8fa543fae30559eebe3ae7a2ad46
Joseph Anderson <joseph_anderson@fitnyc.edu>
2022-07-18 16:39:09 -0400

    Initial commit

---
+++ authorize.js
@@ -0,0 +1,93 @@
+const isObject = require('lodash.isobject');
+const isString = require('lodash.isstring');
+const jwt = require('jsonwebtoken');
+const URI = require('uri-js');
+const AWS = require('aws-sdk');
+const env_vars = require('./env-config.json');
+
+const apiTokenSecret = env_vars.api_token_secret;
+const region = env_vars.region;
+const table = env_vars.table;
+
+function allowedFromRegexes(str) {
+  var configValues = isString(str) ? str.split(';') : [];
+  var result = [];
+  for (var re in configValues) {
+    result.push(new RegExp(configValues[re]));
+  }
+  return result;
+}
+
+function getCurrentUser(token) {
+  if (isString(token)) {
+    try {
+      return jwt.verify(token, apiTokenSecret);
+    } catch (err) {
+      return null;
+    }
+  } else {
+    return null;
+  }
+}
+
+async function authorize(token, id) {
+  const currentUser = getCurrentUser(token);
+  if (isObject(currentUser)) {
+    if (currentUser.visibility == 'public') {
+      console.log('Item ID:', id);
+      console.log('Visibility: public');
+      return true;
+    } else if (currentUser.user != 'Anonymous') {
+      console.log('Item ID:', id);
+      console.log('User:', currentUser.user);
+      return true;
+    }
+  }
+  const username = 'Anonymous';
+  const visibility = await getItemVisibility(id);
+
+  console.log('User:', username);
+  console.log('Item ID:', id);
+  console.log('Visibility:', visibility);
+
+  switch (visibility) {
+    case 'public':
+      return true;
+    case 'private':
+      return false;
+    default:
+      return true;
+  }
+}
+
+async function getItemVisibility(id) {
+  // Set the region
+  AWS.config.update({
+    region: region
+  });
+  const docClient = new AWS.DynamoDB.DocumentClient({
+    apiVersion: '2012-08-10'
+  });
+  try {
+    var params = {
+      TableName: table,
+      Key: {
+        'key': id
+      }
+    };
+    var result = await docClient.get(params).promise();
+    if (!isObject(result)) {
+      return null;
+    }
+    if (result.Item.visibility) {
+      return result.Item.visibility.toLowerCase();
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+module.exports = authorize;
---
+++ index.js
@@ -0,0 +1,121 @@
+const AWS = require('aws-sdk');
+const authorize = require('./authorize');
+const isObject = require('lodash.isobject');
+const isString = require('lodash.isstring');
+
+function getEventHeader(request, name) {
+  if (request.headers && request.headers[name] && request.headers[name].length > 0) {
+    return request.headers[name][0].value;
+  } else {
+    return undefined;
+  }
+}
+
+function getBearerToken(request) {
+  let authHeader = getEventHeader(request, 'authorization');
+  if (isString(authHeader)) {
+    return authHeader.replace(/^Bearer /, '');
+  }
+  return null;
+}
+
+function addAccessControlHeaders(request, response) {
+  response.headers['access-control-allow-origin'] = [{
+    key: 'Access-Control-Allow-Origin',
+    value: '*'
+  }];
+  response.headers['access-control-allow-headers'] = [{
+    key: 'Access-Control-Allow-Headers',
+    value: 'authorization'
+  }];
+  response.headers['access-control-allow-credentials'] = [{
+    key: 'Access-Control-Allow-Credentials',
+    value: 'true'
+  }];
+  return response;
+}
+
+function viewerRequestOptions(request) {
+  const response = {
+    status: '200',
+    statusDescription: 'OK',
+    headers: {},
+    body: 'OK'
+  };
+
+  return addAccessControlHeaders(request, response);
+}
+
+async function viewerRequestIiif(request) {
+  // Check if this is a request for resource itself rather something else that wouldn't need to be authorized (ie endpoint or something that would be redirected)
+  const path = decodeURI(request.uri);
+  const segments = path.split('/');
+  if ((path.startsWith('/iiif/2/')) && (segments.length > 4) && (segments[4])) {
+    const id = decodeURIComponent(segments[3]) + '.tif';
+    const authToken = getBearerToken(request);
+    const authed = await authorize(authToken, id);
+    console.log('Authorized:', authed);
+
+    // Return a 403 response if not authorized to view the requested item
+    if (!authed) {
+      const response = {
+        status: '403',
+        statusDescription: 'Forbidden',
+        body: 'Forbidden'
+      };
+      return response;
+    }
+
+    return request;
+  } else {
+    return request;
+  }
+}
+
+async function processViewerRequest(event) {
+  console.log('Initiating viewer-request trigger')
+  const {
+    request
+  } = event.Records[0].cf;
+  let result;
+
+  if (request.method === 'OPTIONS') {
+    // Intercept OPTIONS request and return proper response
+    result = viewerRequestOptions(request);
+  } else {
+    result = await viewerRequestIiif(request);
+  }
+
+  return result;
+}
+
+async function processViewerResponse(event) {
+  console.log('Initiating viewer-response trigger')
+  const {
+    request,
+    response
+  } = event.Records[0].cf;
+  return addAccessControlHeaders(request, response);
+}
+
+async function processRequest(event, _context, callback) {
+  const {
+    eventType
+  } = event.Records[0].cf.config;
+  let result;
+
+  console.log('Event Type:', eventType);
+  if (eventType === 'viewer-request') {
+    result = await processViewerRequest(event);
+  } else if (eventType === 'viewer-response') {
+    result = await processViewerResponse(event);
+  } else {
+    result = event.Records[0].cf.request;
+  }
+
+  return callback(null, result);
+}
+
+module.exports = {
+  handler: processRequest
+};
